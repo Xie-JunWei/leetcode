c62dd9ab28c9cc900fcbaf304b8eaf3a73d54fe4
谢俊伟 <71635772+Xie-JunWei@users.noreply.github.com>
2021-03-04 11:28:51 +0800

    Add files via upload

---
+++ make matrix zero.cpp
@@ -0,0 +1,79 @@
+#include"iostream"
+#include"vector"
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int temp = 1;
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (matrix[i][j] == 0 && j == 0)
+                {
+                    temp = 0;
+                }
+                else if(matrix[i][j]==0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+
+
+        for (int i = 1; i < row; i++)
+        {
+            if (matrix[i][0] == 0)
+            {
+                for (int j = 1; j < col; j++)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+
+        for (int i = 1; i <col; i++)
+        {
+            if (matrix[0][i] == 0)
+            {
+                for (int j = 1; j < row; j++)
+                {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for (int i = 0; i < col; i++)
+            {
+                matrix[0][i] = 0;
+            }
+        }
+        if (temp == 0)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+
+
+
+
+int main()
+{
+    Solution a;
+    vector<vector<int>> b = { {1,1,1},{1,0,1},{1,1,1 } };
+    a.setZeroes(b);
+}
